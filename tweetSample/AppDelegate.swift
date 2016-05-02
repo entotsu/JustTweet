7d0672cbaba873ef582e604c7dbc2da82f0d2bbe
Takuya Okamoto <blackn.red42@gmail.com>
2016-05-02 20:56:49 +0900

    Initial Commit

---
+++ tweetSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tweetSample
+//
+//  Created by 岡本 拓也 on 2016/05/02.
+//  Copyright © 2016年 岡本 拓也. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ tweetSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tweetSample
+//
+//  Created by 岡本 拓也 on 2016/05/02.
+//  Copyright © 2016年 岡本 拓也. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
